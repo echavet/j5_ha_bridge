dc902bf5bfcaf8952105aef64c8f3d945e58f4ff
Eric Chavet <echavet@gmail.com>
2023-05-30 10:54:33 +0200

    màj

--- terminalInContainer.sh
+++ terminalInContainer.sh
@@ -1,2 +1,6 @@
+sur image:
 docker run -it local/aarch64-addon-configurablefirmata_hassio_bridge:1.1.1 /bin/bash
 docker run -it local/aarch64-addon-j5_ha_bridge:1.1.16 /bin/bash
+
+sur container :
+docker exec -it f823f0ccd00f /bin/bash
